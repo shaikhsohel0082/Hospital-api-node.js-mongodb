1554e3faa72b1a3b14eaee997de12c0659cc37d5
shaikhsohel0082 <142989264+shaikhsohel0082@users.noreply.github.com>
2024-03-12 22:12:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/hospital';
 
-mongoose
-  .connect(MONGODB_URI)
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => {
     console.log("Connected to MongoDB");
     app.use(express.json());
