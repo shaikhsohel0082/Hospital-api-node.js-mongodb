800162cef6a3a33fa100b4591d8cb619a2244e39
shaikhsohel0082 <142989264+shaikhsohel0082@users.noreply.github.com>
2024-03-12 22:10:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/hospital';
 
 mongoose
   .connect(MONGODB_URI)
