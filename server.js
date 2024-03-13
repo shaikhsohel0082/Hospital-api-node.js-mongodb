93badd9297fa1bbfb928846a560d8db6a74d7dae
shaikhsohel0082 <142989264+shaikhsohel0082@users.noreply.github.com>
2024-03-13 11:39:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ mongoose.connect(MONGODB_URI, {
     console.log("Connected to MongoDB");
     app.use(express.json());
     app.use("/api", mainRouter);
+     app.get("/", (req, res) => {
+      res.send("Welcome to Hospital Managment API ");
+    });
 
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
